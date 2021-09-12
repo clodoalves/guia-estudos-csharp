c23b74bb95a24946557d764c2549e8ae694f86fb
José Clodoalves <jrjcsj@gmail.com>
2021-09-12 09:23:58 -0300

    Exemplo de SortedList

---
+++ Colecoes/SortedListSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace SortedListSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList lista = new SortedList();
+
+            lista.Add(4, "Quarto");
+            lista.Add(2, "Segundo");
+            lista.Add(1, "Segundo");
+            lista.Add(3, "Terceiro");
+
+            foreach (object chave in lista.Keys)
+            {
+                Console.WriteLine("{0}-{1}", chave.ToString(), lista[chave].ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
