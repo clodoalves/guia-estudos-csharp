92a74a3c416dd3a1e314feefe318a77e994730e9
José Clodoalves <jrjcsj@gmail.com>
2021-09-12 21:15:47 -0300

    Exemplo de Queue e Stack

---
+++ Colecoes/QueueEStackSample/FifoSample.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Text;
+
+namespace QueueEStackSample
+{
+    public class FifoSample
+    {
+        public Queue FilaAtendimento;
+
+        public void PreencherFila() 
+        {
+            FilaAtendimento.Enqueue("Joao");
+            FilaAtendimento.Enqueue("Maria");
+            FilaAtendimento.Enqueue("Francisca");
+            FilaAtendimento.Enqueue("Marcelo");
+        }
+
+        public void AtenderProximoCliente()
+        {
+            if (FilaAtendimento.Count > 0) 
+            {
+                FilaAtendimento.Dequeue();
+            } 
+        }
+
+        public string ObterProximoCliente()
+        {
+            string proximoCliente = string.Empty;
+
+            if (FilaAtendimento.Count > 0) 
+            {
+                proximoCliente = FilaAtendimento.Peek().ToString();
+            }
+            
+             return proximoCliente;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (string item in FilaAtendimento)
+            {
+               sb.AppendLine(item);   
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Colecoes/QueueEStackSample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueEStackSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecutarLifoSample();
+            Console.ReadLine();
+        }
+
+        public static void ExecutarFifoSample()
+        {
+            FifoSample fifoSample = new FifoSample();
+
+            fifoSample.FilaAtendimento = new Queue();
+
+            fifoSample.PreencherFila();
+
+            Console.WriteLine("Lista de clientes:");
+            Console.WriteLine(fifoSample);
+
+            Console.WriteLine($"Atendendo próximo cliente: {fifoSample.ObterProximoCliente()}");
+            fifoSample.AtenderProximoCliente();
+
+            Console.WriteLine("Lista de clientes atualizada:");
+            Console.WriteLine(fifoSample);
+
+            Console.WriteLine($"Atendendo próximo cliente: {fifoSample.ObterProximoCliente()}");
+            fifoSample.AtenderProximoCliente();
+
+            Console.WriteLine("Lista de clientes atualizada:");
+            Console.WriteLine(fifoSample);
+        } 
+
+        public static void ExecutarLifoSample()
+        {
+            LifoSample lifoSample = new LifoSample();
+
+            lifoSample.PilhaPratos = new Stack();
+
+            lifoSample.PreencherPilha();
+
+            Console.WriteLine("Pilha de pratos:");
+            Console.WriteLine(lifoSample);
+
+            Console.WriteLine($"Retirando próximo prato: {lifoSample.ObterProximoPrato()}");
+            lifoSample.RetirarProximoPrato();
+
+            Console.WriteLine("Pilha de pratos atualizada:");
+            Console.WriteLine(lifoSample);
+            
+            Console.WriteLine($"Retirando próximo prato: {lifoSample.ObterProximoPrato()}");
+            lifoSample.RetirarProximoPrato();
+
+            Console.WriteLine("Pilha de pratos atualizada:");
+            Console.WriteLine(lifoSample);                      
+        }
+    }
+}
