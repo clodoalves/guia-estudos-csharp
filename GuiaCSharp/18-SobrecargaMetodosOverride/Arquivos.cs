b97df8da2278b720debbb1bf0a0fbe69f8eded9f
Jose Clodoalves <jrjcsj@gmail.com>
2021-11-07 21:03:03 -0300

    Criando pequeno framework para abertura de arquivos sem uso de sobrecargada de metodos com override.

---
+++ GuiaCSharp/18-SobrecargaMetodosOverride/Arquivos.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaMetodosOverride
+{
+    public class Musica 
+    {
+        public string Nome;
+        public double Tamanho;
+        public void Abrir() 
+        {
+            Console.WriteLine($"Tocando musica: {Nome}");
+        }
+
+       
+    }
+
+    public class DocumentoWord
+    {
+        public string Nome;
+        public double Tamanho;
+
+        public void Abrir()
+        {
+            Console.WriteLine($"Abrindo documento: {Nome}");
+        }
+    }
+
+    public class Imagem
+    {
+        public string Nome;
+        public double Tamanho;
+
+        public void Abrir()
+        {
+            Console.WriteLine($"Exibindo imagem: {Nome}");
+        }
+    }
+
+    public class PlanilhaExcel
+    {
+        public string Nome;
+        public double Tamanho;
+
+        public void Abrir()
+        {
+            Console.WriteLine($"Abrindo planilha: {Nome}");
+        }
+    }
+
+    public class Windows
+    {
+        private ArrayList _arquivos;
+
+        public Windows()
+        {
+            _arquivos = new ArrayList();
+        }
+
+        public void AdicionarArquivo(object arquivo)
+        {
+            _arquivos.Add(arquivo);
+        }
+
+        public void AbrirArquivo(object arquivo)
+        {
+            if (arquivo is Musica musica)
+            {
+                musica.Abrir();
+            }
+            else if (arquivo is DocumentoWord documentoWord)
+            {
+                documentoWord.Abrir();
+            }
+            else if (arquivo is Imagem imagem)
+            {
+                imagem.Abrir();
+            }
+            else if (arquivo is PlanilhaExcel planilhaExcel)
+            {
+                planilhaExcel.Abrir();
+            }
+        }
+    }
+}
---
+++ GuiaCSharp/18-SobrecargaMetodosOverride/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaMetodosOverride
+{
+    /// <summary>
+    /// Este exemplo demonstra:
+    /// <para>Como trabalhar com sobrecarga de metodos (sobreescrita) usando override, que permite ter
+    /// multiplas versoes do mesmo metodo, matendo o mesmo nome</para>
+    /// <para>Override, diferente de overload, necessita o uso de heranca, 
+    /// aplicando-se a tecnica de polimorfismo</para>
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Windows windows = new Windows();
+
+            Musica musica = new Musica { Nome = "Pink Floyd - Learning To Fly.mp3" };
+            DocumentoWord documentoWord = new DocumentoWord { Nome = "Trabalho da faculdade.doc" };
+            Imagem imagem = new Imagem { Nome = "Floresta Amazonica.jpg" };
+            PlanilhaExcel planilhaExcel = new PlanilhaExcel { Nome = "Planilha de gastos.xls" };
+
+            windows.AdicionarArquivo(musica);
+            windows.AdicionarArquivo(documentoWord);
+            windows.AdicionarArquivo(imagem);
+            windows.AdicionarArquivo(planilhaExcel);
+
+            windows.AbrirArquivo(musica);
+            windows.AbrirArquivo(documentoWord);
+            windows.AbrirArquivo(imagem);
+            windows.AbrirArquivo(planilhaExcel);
+
+            Console.ReadLine();
+        }
+    }
+}
