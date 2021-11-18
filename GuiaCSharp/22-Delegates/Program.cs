a3834c806d95da2b616fbf8fd664ef24b4aeba46
Jose Clodoalves <jrjcsj@gmail.com>
2021-11-18 20:51:57 -0300

    Utilizando delegate para definição de novos tipos de delegações

---
+++ GuiaCSharp/22-Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedido = new Pedido();
+            pedido.Pagar += new PagarEvent(Pedido_Pagar);
+            pedido.Fechar(500);
+            Console.ReadLine();
+        }
+
+        private static void Pedido_Pagar(double valor)
+        {
+            Console.WriteLine($"Boleto pago no valor de {valor}");
+        }
+    }
+}
