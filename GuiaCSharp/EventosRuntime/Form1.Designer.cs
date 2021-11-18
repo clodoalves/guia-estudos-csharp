6cdc57d4cccd0ba10c0b03aa61ca84acdc7045f5
Jose Clodoalves <jrjcsj@gmail.com>
2021-11-18 16:03:29 -0300

    Exemplo de criação de eventos em tempo de execução

---
+++ GuiaCSharp/EventosRuntime/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace EventosRuntime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCriarOutroBotao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCriarOutroBotao
+            // 
+            this.btnCriarOutroBotao.Location = new System.Drawing.Point(259, 136);
+            this.btnCriarOutroBotao.Name = "btnCriarOutroBotao";
+            this.btnCriarOutroBotao.Size = new System.Drawing.Size(201, 30);
+            this.btnCriarOutroBotao.TabIndex = 0;
+            this.btnCriarOutroBotao.Text = "Criar outro botao";
+            this.btnCriarOutroBotao.UseVisualStyleBackColor = true;
+            this.btnCriarOutroBotao.Click += new System.EventHandler(this.btnCriarOutroBotao_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCriarOutroBotao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCriarOutroBotao;
+    }
+}
+
---
+++ GuiaCSharp/EventosRuntime/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventosRuntime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCriarOutroBotao_Click(object sender, EventArgs e)
+        {
+            Button dynamicButton = new Button();
+            dynamicButton.Parent = this;
+            dynamicButton.Location = new Point(235, 150);
+            dynamicButton.Text = "Novo botão";
+
+            dynamicButton.Click += new EventHandler(DynamicButton_Click);
+
+            //Método anônimo
+            dynamicButton.Click += delegate (object s, EventArgs ea)
+            {
+                MessageBox.Show("Ola mundo 2");
+            };
+
+            //Lambda expression
+            dynamicButton.Click += (s, ea) =>
+            {
+                MessageBox.Show("Ola mundo 3");
+            };
+        }
+
+        private void DynamicButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ola mundo");
+        }
+    }
+}
