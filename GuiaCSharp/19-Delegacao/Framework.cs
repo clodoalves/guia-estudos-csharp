0a34ea50b1ee79d68b70690144c88d164b929f03
Jose Clodoalves <jrjcsj@gmail.com>
2021-11-18 10:39:53 -0300

    Reestruturação de pastas. Criação de projeto para exemplo prático de uso de delegação em C#

---
+++ GuiaCSharp/19-Delegacao/Framework.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegacao
+{
+    public class Boleto 
+    {
+        public void Pagar(double valor) 
+        {
+            Console.WriteLine($"Boleto pago no valor de {valor}");
+        }
+    }
+
+    public class Pedido
+    {
+        private Boleto _boleto = new Boleto();
+
+        public void Fechar(double valor) 
+        {
+            //delegacao
+            _boleto.Pagar(valor);
+        }
+    }
+}
---
+++ GuiaCSharp/19-Delegacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedido = new Pedido();
+
+            pedido.Fechar(2000);
+
+            Console.ReadLine();
+        }
+    }
+}
