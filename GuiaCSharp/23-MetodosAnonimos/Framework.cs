89a24a85094c4faca51d1fea20675e155d8f3463
Jose Clodoalves <jrjcsj@gmail.com>
2021-11-18 21:27:46 -0300

    Utilizando métodos anônimos com Delegates e eventos

---
+++ GuiaCSharp/23-MetodosAnonimos/Framework.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MetodosAnonimos
+{
+    public delegate void PagarEvent(double valor);
+
+    public class Pedido
+    {
+        public event PagarEvent Pagar;
+        public void Fechar(double valor) 
+        {
+            //delegação
+            this.Pagar(valor);
+        }
+    }
+}
---
+++ GuiaCSharp/23-MetodosAnonimos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosAnonimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedido = new Pedido();
+            //pedido.Pagar += new PagarEvent(Pedido_Pagar);
+            pedido.Pagar += delegate (double valor)
+            {
+                Console.WriteLine($"Boleto pago no valor de {valor}");
+            };
+
+            pedido.Fechar(500);
+            Console.ReadLine();
+        }
+
+        private static void Pedido_Pagar(double valor)
+        {
+            Console.WriteLine($"Boleto pago no valor de {valor}");
+        }
+    }
+}
