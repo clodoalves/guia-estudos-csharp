3ab1bfb90fb586bf6d5cbb32b6352c7880e4e771
Jose Clodoalves <jrjcsj@gmail.com>
2022-05-16 17:45:46 -0300

    Exemplo de propagação de exceptions com throw

---
+++ GuiaCSharp/PropagandoExceptionsComThrow/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_PropagandoExceptionsComThrow
+{
+    class Program
+    {
+        public static void ChamarRepositorio(string tarefa) 
+        {
+            DateTime data = Convert.ToDateTime(tarefa);
+        }
+        
+        public static void ChamarServico(string tarefa) 
+        {
+            try
+            {
+                ChamarRepositorio(tarefa);
+            }
+            catch (FormatException e)
+            {
+               throw new Exception("Serviço detectou erro: "+e.Message);
+            }
+        }
+
+        public static void ChamarRegraNegocio(string tarefa) 
+        {
+            try
+            {
+                ChamarServico(tarefa);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ocorreu um erro interno");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string tarefa = "99/99/9999";
+            Console.WriteLine("Salvar data...");
+            ChamarRegraNegocio(tarefa);
+
+            Console.ReadLine();
+        }
+    }
+}
