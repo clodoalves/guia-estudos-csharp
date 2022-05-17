d9ca0057a18959547b69e4534d1488ab89ca3263
Jose Clodoalves <jrjcsj@gmail.com>
2022-05-16 22:56:39 -0300

    Exemplo com Custom Exceptions

---
+++ GuiaCSharp/CriandoCustomExceptions/Autentificacao.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_CriandoCustomExceptions
+{
+    public class CampoInvalidoException : Exception 
+    {
+        public CampoInvalidoException(string mensagem) : base(mensagem)
+        {
+
+        }    
+    }
+
+    public class TotalCaracteresExcedidoException : Exception 
+    {
+        public TotalCaracteresExcedidoException(string mensagem) : base(mensagem)
+        {
+
+        }
+    }
+
+
+    public class SenhaInvalidaException : Exception 
+    {
+        public SenhaInvalidaException(string mensagem) : base(mensagem)
+        {
+
+        }  
+    
+    } 
+
+    public static class Autentificacao
+    {
+        private const int LIMITE_CARACTERES_CAMPO_USUARIO = 15;
+        private const int LIMITE_CARACTERES_CAMPO_SENHA = 10;
+
+        public static void Autenticar(string usuario, string senha)
+        {
+
+            if (usuario.Length > LIMITE_CARACTERES_CAMPO_USUARIO)
+            {
+                throw new TotalCaracteresExcedidoException("O limite de caracteres para o campo Usuário é 15");
+            }
+
+            if (usuario.Length > LIMITE_CARACTERES_CAMPO_SENHA) 
+            {
+                throw new TotalCaracteresExcedidoException("O limite de caracteres para o campo Senha é 10");
+            }
+
+            if (usuario != "admin" || senha != "admin")
+            {
+                throw new CampoInvalidoException("Usuário inválido ou Senha inválida");
+            } 
+        }
+    }
+}
---
+++ GuiaCSharp/CriandoCustomExceptions/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace _32_CriandoCustomExceptions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(89, 83);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(57, 17);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(91, 141);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(49, 17);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "Senha";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(157, 83);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 22);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(157, 141);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 22);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(94, 196);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 31);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
