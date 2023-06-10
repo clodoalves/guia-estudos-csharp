26d48b5e92074c1f46760b6c81c5416ab500a5e1
Jose Clodoalves <jrjcsj@gmail.com>
2023-06-10 20:48:37 -0300

    Implementando IDisposable

---
+++ GuiaCSharp/35-ImplementandoIDisposible/Conexao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _35_ImplementandoIDisposible
+{
+    public class Conexao : IDisposable
+    {
+        public string Handle;
+        
+        public Conexao()
+        {
+            Handle = "Recurso em uso";
+            Console.WriteLine("Recurso alocado");     
+        }
+
+        public void Abrir()
+        {
+            Console.WriteLine("Conexao aberta");
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Conexao fechada");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+
+        }
+
+        /// <summary>
+        /// Simula liberacao de recurso nao gerenciado pela CLR
+        /// </summary>
+        private void LiberarRecurso()
+        {
+            Handle = "";
+            Console.WriteLine("Recurso liberado com sucesso");
+        }
+
+        public virtual void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                //libera recursos gerenciados pela CLR
+            }
+            
+            //libera recursos nao gerenciados pela CLR
+            LiberarRecurso();
+        }
+    }
+}
---
+++ GuiaCSharp/35-ImplementandoIDisposible/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _35_ImplementandoIDisposible
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Conexao conexao = new Conexao())
+            {
+                conexao.Abrir();
+                conexao.Fechar();
+            }
+
+            #region Usando try-finally
+            // Conexao conexao = new Conexao();
+            //     
+            // try
+            // {
+            //     conexao.Abrir();
+            //     conexao.Fechar();
+            // }
+            // finally
+            // {
+            //     if (conexao != null)
+            //         conexao.Dispose();
+            // 
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
