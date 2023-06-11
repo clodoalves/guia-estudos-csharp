fd9b9048a433983e9f569aabad3ca0ffa6e6a7af
Jose Clodoalves <jrjcsj@gmail.com>
2023-06-10 21:35:26 -0300

    Implementando IEnumerable

---
+++ GuiaCSharp/36-ImplementandoIEnumerable/ListaClientes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ImplementandoIEnumerable
+{
+    public class ListaClientes : IEnumerable
+    {
+        private List<string> _clientes = new List<string>();
+        private int _index;
+
+        public void Add(string cliente)
+        {
+            _clientes.Add(cliente);
+            _index += 1;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < _index; i++)
+            {
+                yield return _clientes[i];
+            }
+        }
+    }
+}
---
+++ GuiaCSharp/36-ImplementandoIEnumerable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ImplementandoIEnumerable
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListaClientes clientes = new ListaClientes();
+            
+            clientes.Add("Joao");
+            clientes.Add("Jose");
+            clientes.Add("Maria");
+            clientes.Add("Severina");
+
+            foreach (string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
