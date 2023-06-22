168ab1a2e199c1783134e3ba56e3c381a6835e45
José Clodoalves <jrjcsj@gmail.com>
2023-06-21 22:04:06 -0300

    Exemplo com ArrayList

---
+++ GuiaCSharp/37-ArrayList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+
+namespace _37_ArrayList
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add("Joao");
+            arrayList.Add("Jose");
+            arrayList.Add("Maria");
+            arrayList.Add("Severina");
+            arrayList.Add(20);
+            arrayList.Add(new DataSet());
+
+            foreach (var element in arrayList)
+            {
+                Console.WriteLine(element);
+            }
+            
+            Console.WriteLine($"Array count: {arrayList.Count}");
+            Console.WriteLine($"Array capacity: {arrayList.Capacity}");
+            
+            Console.ReadLine();
+        }
+    }
+}
