30d5eb88a7ceeea90226520e6392013cd8e3df27
Jose Clodoalves <jrjcsj@gmail.com>
2023-09-18 21:41:08 -0300

    Adicionando metodos para obter informacoes de métodos, propriedades e atributos por reflection

--- GuiaCSharp/ReflexaoClasses/Program.cs
+++ GuiaCSharp/ReflexaoClasses/Program.cs
@@ -11,129 +11,149 @@ namespace ReflexaoClasses
     {
         static void Main(string[] args)
         {
-           var propriedades = ObterNomeEValorPropriedade();
-
-           foreach (var propriedade in propriedades)
-           {
-               Console.WriteLine($"{propriedade.Key} - {propriedade.Value}");
-           }
-           
-           Console.ReadLine();
-        }
-
-        private static Dictionary<string, object> ObterNomeEValorAtributo()
-        {
-            Dictionary<string, object> attributesInformation = new Dictionary<string, object>();
             Carro carro = new Carro();
-
+            
             carro.Cor = "Amarelo";
             carro.Modelo = "Modelo A";
             carro.Marca = "Marca 1";
             
             carro.Acelerar(200);
 
+           var propriedades = ObterParametrosMetodos(carro);
+
+           foreach (var propriedade in propriedades)
+           {
+               Console.WriteLine(propriedade);
+           }
+           
+           Console.ReadLine();
+        }
+
+        private static List<string> ObterParametrosMetodos(Carro carro)
+        {
+            List<string> nomesParametrosMetodos = new List<string>();
+            
+            Type tipo = typeof(Carro);
+
+            MethodInfo[] infoMetodos = tipo.GetMethods();
+
+            foreach (var infoMetodo in infoMetodos)
+            {
+                ParameterInfo[] infoParametros = infoMetodo.GetParameters();
+
+                foreach (var infoParametro in infoParametros)
+                {
+                    nomesParametrosMetodos.Add($"Metodo {infoMetodo.Name} - Parametro {infoParametro.Name}");
+                }
+            }
+            
+            return nomesParametrosMetodos;
+        }
+
+        private static List<string> ObterInterfaces(Carro carro)
+        {
+            List<string> nomesInterfaces = new List<string>();
+
+            Type tipo = typeof(Carro);
+
+            Type [] interfaces = tipo.GetInterfaces();
+            
+            foreach (var tipoInterface in interfaces)
+            {
+                nomesInterfaces.Add(tipoInterface.FullName);
+            }
+            
+            return nomesInterfaces;
+        }
+
+        private static List<string> ObterNomesMetodos(Carro carro)
+        {
+            List<string> nomesMetodos = new List<string>();
+
             Type tipo = carro.GetType();
 
-            FieldInfo[] fieldInfos = tipo.GetFields();
+            MethodInfo[] infoMetodos = tipo.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public);
 
-            foreach (var fieldInfo in fieldInfos)
+            foreach (var infoMetodo in infoMetodos)
+            {
+                nomesMetodos.Add(infoMetodo.Name);
+            }
+            
+            return nomesMetodos;
+        }
+        
+        private static Dictionary<string, object> ObterInformacoesAtributos(Carro carro)
+        {
+            Dictionary<string, object> informacoesAtributos = new Dictionary<string, object>();
+            
+            Type tipo = carro.GetType();
+
+            FieldInfo[] infoAtributos = tipo.GetFields();
+
+            foreach (var fieldInfo in infoAtributos)
             {
                 string nome = fieldInfo.Name;
                 var valor = tipo.GetField(fieldInfo.Name).GetValue(carro);
-                attributesInformation[nome] = valor;
-                //Console.WriteLine($"{fieldInfo.Name} - {valor}");
+                informacoesAtributos[nome] = valor;
             }
 
-            return attributesInformation;
+            return informacoesAtributos;
         }
 
-        private static Dictionary<string, object> ObterNomeEValorPropriedade()
+        private static Dictionary<string, object> ObterInformacoesPropriedades(Carro carro)
         {
-            Dictionary<string, object> propertyInformation = new Dictionary<string, object>();
-            Carro carro = new Carro();
-
-            carro.Cor = "Cinza";
-            carro.Modelo = "Modelo B";
-            carro.Marca = "Marca 3";
-            
-            carro.Acelerar(200);
-
+            Dictionary<string, object> informacoesPropriedades = new Dictionary<string, object>();
+             
             Type tipo = carro.GetType();
 
-            PropertyInfo[] propertyInfos = tipo.GetProperties();
+            PropertyInfo[] infoPropriedades = tipo.GetProperties();
 
-            foreach (var propertyInfo in propertyInfos)
+            foreach (var propertyInfo in infoPropriedades)
             {
                 string nome = propertyInfo.Name;
                 var valor = tipo.GetProperty(propertyInfo.Name).GetValue(carro);
-                propertyInformation[nome] = valor;
-                
-                //Console.WriteLine($"{propertyInfo.Name} - {valor}");
+                informacoesPropriedades[nome] = valor;
             }
 
-            return propertyInformation;
+            return informacoesPropriedades;
         }
         
-        private static Dictionary<string, object> ObterNomeEValorMetodo()
-        {
-            Dictionary<string, object> propertyInformation = new Dictionary<string, object>();
-            Carro carro = new Carro();
-
-            carro.Cor = "Cinza";
-            carro.Modelo = "Modelo B";
-            carro.Marca = "Marca 3";
-            
-            carro.Acelerar(200);
-
-            Type tipo = carro.GetType();
-
-            MethodInfo[] methodInfos = tipo.GetMethods();
-
-            foreach (var methodInfo in methodInfos)
-            {
-                string nome = methodInfo.Name;
-                var valor = tipo.GetMethod(methodInfo.Name).GetParameters();
-                propertyInformation[nome] = valor;
-                
-                //Console.WriteLine($"{propertyInfo.Name} - {valor}");
-            }
-
-            return propertyInformation;
-        }
-        
-        
-        private static void ObterPropriedades(Type t)
+        private static void ObterPropriedadesTipo(Type t)
         {
             StringBuilder str = new StringBuilder();
             str.AppendLine("Informações do tipo: " + t.Name);
             str.AppendLine("Nome completo: " + t.FullName);
             str.AppendLine("Namespace: " + t.Name);
             Type tipoBase = t.BaseType;
+           
             if (tipoBase != null)
             {
                 str.AppendLine("Tipo base: " + t.BaseType.Name);
+                
             }
+            
             MemberInfo[] membros = t.GetMembers();
             foreach (var m in membros)
             {
                 str.AppendLine(m.MemberType + " " + m.Name);
             }
+            
             Console.WriteLine(str);
         }
 
-        private static void Exemplo3()
+        private static void ObterTipoExemplo3()
         {
             Type tipo = typeof(Carro);
             Console.WriteLine(tipo.FullName);
         }
 
-        private static void Exemplo2()
+        private static void ObterTipoExemplo2()
         {
             Type tipo = Type.GetType("ReflexaoClasses.Carro", false, true);
             Console.WriteLine(tipo.FullName);
         }
 
-        private static void Exemplo1()
+        private static void ObterTipoExemplo1()
         {
             Carro carro = new Carro();
             Type tipo = carro.GetType();
