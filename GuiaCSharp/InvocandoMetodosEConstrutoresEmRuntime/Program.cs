1745d567cf4a07e1b3c4a8ebf7ba46ec530bbb3d
Jose Clodoalves <jrjcsj@gmail.com>
2023-09-19 21:15:37 -0300

    Implementando exemplo para invocar metodos e construtores em runtime por reflection

---
+++ GuiaCSharp/InvocandoMetodosEConstrutoresEmRuntime/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace InvocandoMetodosEConstrutoresEmRuntime
+{
+    public class Cliente
+    {
+        private string _nome;
+        
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public Cliente(string nome)
+        {
+            _nome = nome;
+        }
+        public void ImprimirSaudacao(string saudacao)
+        {
+            Console.WriteLine($"{saudacao}, {this.Nome}");
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Type tipo = Type.GetType("InvocandoMetodosEConstrutoresEmRuntime.Cliente");
+
+            ConstructorInfo infoConstrutor = tipo.GetConstructor(new Type[] { typeof(string) });
+
+            var objeto = infoConstrutor.Invoke(new object[] { "Joao" });
+
+            MethodInfo methodInfo = tipo.GetMethod("ImprimirSaudacao");
+
+            methodInfo.Invoke(objeto, new object[] { "Bom dia" });
+
+            Console.ReadLine();
+        }
+    }
+}
